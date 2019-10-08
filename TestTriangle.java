dd99557e12e570293d2ead653178be16d21e120e
batataskae El <50258296+laibanasir@users.noreply.github.com>
2019-10-08 11:55:52 +0500

    Add files via upload
    input with setters

---
+++ TestTriangle.java
@@ -0,0 +1,10 @@
+class TestTriangle
+{
+  public static void main(String args[])
+  {
+    Triangle t1 = new Triangle();
+    t1.setS1();
+    t1.setS2();
+    System.out.println("The hypotenuse of this triangle is " + t1.getHyp());
+  }
+}
